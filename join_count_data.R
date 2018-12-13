43d96c44a1c1eac65ae2342aed79323698a009b4
Mason Fidino <mfidino@lpzoo.org>
2018-12-13 13:45:20 -0600

    cleaned the data

--- join_count_data.R
+++ join_count_data.R
@@ -7,16 +7,22 @@ mod_day_seen$ModernName <- clean_names(mod_day_seen$ModernName)
 
 mod_day_seen$ob <- "Fidino"
 mod_arrival <- read.csv("./data/modern_arrival.csv",
-												stringsAsFactors = FALSE)
-
+												stringsAsFactors = FALSE)[,-1]
+mod_arrival$arrival <- ymd(mod_arrival$arrival)
 mod_arrival$ob <- "Fidino"
+mod_arrival$julian <- as.numeric(format(mod_arrival$arrival, format="%j"))
+
+
 
 # bring in the historic data
 
 hist_day_seen <- read.csv("./data/historic_days_to_join.csv",
 													stringsAsFactors = FALSE)
+hist_arrival <- read.csv("./data/historic_arrival_to_join.csv",
+														 stringsAsFactors = FALSE)[,-1]
 
 colnames(hist_day_seen) <- colnames(mod_day_seen)
+colnames(hist_arrival) <- colnames(mod_arrival)
 
 # add zeros if needed
 
@@ -29,12 +35,12 @@ sp_table$ModernName[sp_table$ModernName == "lousiana water thrush"] <-
 
 sp_table$BooksCommonName <- clean_names(sp_table$BooksCommonName)
 sp_table$ModernName <- clean_names(sp_table$ModernName)
-unq_spe <- sort(unique(hist_day_seen$ModernName))
+unq_spe <- sort(unique(hist_arrival$ModernName))
 
 for(species in 1:length(unq_spe)){
 	if(unq_spe[species] %in% sp_table$BooksCommonName){
 		loc <- which(sp_table$BooksCommonName == unq_spe[species])
-		hist_day_seen$ModernName[hist_day_seen$ModernName == unq_spe[species]] <-
+		hist_arrival$ModernName[hist_arrival$ModernName == unq_spe[species]] <-
 			sp_table$ModernName[loc]
 	} else {
 		
@@ -43,7 +49,7 @@ for(species in 1:length(unq_spe)){
 }
 
 
-unq_spe <- sort(unique(hist_day_seen$ModernName))
+unq_spe <- sort(unique(hist_arrival$ModernName))
 years <- sort(unique(hist_day_seen$Year))
 	tmp <- data.frame(ModernName = rep(unq_spe, each = length(years)),
 									Year = rep(years, length(unq_spe)),
@@ -67,7 +73,7 @@ test$lowername[test$lowername == "harriss sparrow"] <- "harris sparrow"
 
 test <- data.frame(apply(test, 2, as.character), stringsAsFactors = FALSE)
 
-test$COMMONNAME[2045] <- "Golden-winged Warbler"
+test <- test[-2045,]
 
 write.csv(test, "./data/species_codes.csv", row.names = FALSE)
 
@@ -77,3 +83,25 @@ hm <- left_join(dayseen, test[,c("COMMONNAME", "lowername")],
 unq_spe <- sort(unique(hm$COMMONNAME))
 
 write.csv(dayseen, "./data/clean_days_seen_per_year.csv", row.names = FALSE)
+
+sp_code <- read.csv("./data/species_codes.csv", stringsAsFactors = FALSE)
+
+sp_code <- sp_code[which(sp_code$lowername %in% dayseen$ModernName),]
+
+both_arrival <- rbind(mod_arrival, hist_arrival)
+
+for(species in 1:nrow(sp_code)){
+	if(any(dayseen$ModernName == sp_code$lowername[species])){
+	dayseen$ModernName[dayseen$ModernName == sp_code$lowername[species]] <- 
+		sp_code$COMMONNAME[species]
+	}
+	if(any(mod_arrival$ModernName == sp_code$lowername[species])){
+		both_arrival$ModernName[both_arrival$ModernName == sp_code$lowername[species]] <-
+			sp_code$COMMONNAME[species]
+	}
+	}
+
+write.csv(dayseen, "./data/clean_days_seen_per_year.csv", row.names = FALSE)
+write.csv(both_arrival, "./data/clean_arrival.csv", row.names = FALSE)
+
+
